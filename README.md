6ffe70c0a5e868da0488c1f43ef2d739fc113023
David Langheiter <david@langheiter.com>
2018-11-07 20:39:19 +0100

    Updated documentation

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Backend for the Project [MEVA](https://github.com/MEVA-ITP)
 
 Backend using:
+* [Express](https://expressjs.com/)
 * [Falcor](https://netflix.github.io/falcor/)
+* [ldapjs](http://ldapjs.org/)
--- src/general/router.js
+++ src/general/router.js
@@ -1,38 +1,34 @@
 import Router from 'falcor-router'
-import {database, models} from "../database/database";
 
-class _MEVARouter extends
-
-    Router.createClass([
-        {
-            route: 'status',
-            get: function (pathSet) {
-                return {path: ['status'], value: this.user ? 'authenticated' : 'unauthenticated'}
-            }
-        },
-        {
-            route: "user['email', 'phone', 'external', 'active']",
-            get: async function (pathSet) {
-                let keys = pathSet[1]
-                if(!this.user) {
-                    throw new Error('not authorized')
-                }
-
-                return keys.map(key => ({path: ['user', key], value: this.user[key]}))
-            }
-        },
-        {
-            route: "userById[{keys:ids}]['email', 'phone', 'external', 'active', 'message_tokens', 'password']",
-            get: function *(pathSet) {
-                if(!this.isLocalhost) {
-                    throw new Error("not authorized")
-                }
-
-
-
-            }
+class _MEVARouter extends Router.createClass([
+    {
+        route: 'status',
+        get: function (pathSet) {
+            return {path: ['status'], value: this.user ? 'authenticated' : 'unauthenticated'}
         }
-    ]) {
+    },
+    {
+        route: "user['email', 'phone', 'external', 'active']",
+        get: async function (pathSet) {
+            let keys = pathSet[1]
+            if (!this.user) {
+                throw new Error('not authorized')
+            }
+
+            return keys.map(key => ({path: ['user', key], value: this.user[key]}))
+        }
+    },
+    {
+        route: "userById[{keys:ids}]['email', 'phone', 'external', 'active', 'message_tokens', 'password']",
+        get: function* (pathSet) {
+            if (!this.isLocalhost) {
+                throw new Error("not authorized")
+            }
+
+
+        }
+    }
+]) {
 
     constructor(user, isLocalhost) {
         super();
