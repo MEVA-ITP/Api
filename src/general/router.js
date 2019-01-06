ff533d4726baa0fa4800d8a42835ff4676b7f503
David Langheiter <david@langheiter.com>
2019-01-06 14:48:26 +0100

    Moved loadRoutes to router

--- src/general/router.js
+++ src/general/router.js
@@ -1,5 +1,28 @@
 import Router from 'falcor-router'
-import {loadRoutes} from "./routes";
+import fs from "fs";
+import path from "path";
+import {config} from "../config/config";
+
+export const loadRoutes = () => {
+    let ret = []
+
+    // Read all files in routes directory
+    fs.readdirSync(path.join(config.paths.routes)).sort().forEach(file => {
+        // Check if file name ends with '.route.js'
+        if (file.endsWith(".route.js")) {
+            // Load route from file
+            const {route} = require(path.join('..', config.paths.routes, file))
+            // Check if route is defined
+            if (route !== undefined && route !== null) {
+                console.log("LOADED ROUTE:", file)
+                // Add route/s to ret
+                ret = ret.concat(Array.isArray(route) ? route : [route])
+            }
+        }
+    })
+
+    return ret
+}
 
 let MEVARouterBase = Router.createClass(loadRoutes())
 
