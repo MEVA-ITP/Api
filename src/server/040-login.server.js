0c7586c929151e939d1ce0eed852230ed3228334
David Langheiter <david@langheiter.com>
2019-01-11 14:34:34 +0100

    Added some logging

--- src/server/040-login.server.js
+++ src/server/040-login.server.js
@@ -1,18 +1,22 @@
 import passport from "passport";
+import {logger} from "../general/loger";
 
 export const init = (app) => {
     // Use auth strategy
     app.post('/login', (req, res, next) => {
         passport.authenticate('auth', (err, user, info) => {
             if (err) {
-                return next(err)
+                logger.error(err)
+                return res.send(JSON.stringify({ok: false, error: "Internal error."}))
             }
             if (!user) {
+                logger.warn(`Unsuccessful login. Info: ${info}`)
                 return res.send(JSON.stringify({ok: false, error: "Wrong credentials."}))
             }
             req.login(user, (err) => {
                 if (err) {
-                    return next(err)
+                    logger.error(err)
+                    return res.send(JSON.stringify({ok: false, error: "Internal error."}))
                 }
                 return res.send(JSON.stringify({ok: true, error: null}))
             })
