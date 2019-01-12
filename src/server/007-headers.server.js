2e86b235ca3c83f1d196831c14db35d6bf939f87
David Langheiter <david@langheiter.com>
2019-01-12 13:02:29 +0100

    Added Access-Control-Allow-Origin header

---
+++ src/server/007-headers.server.js
@@ -0,0 +1,7 @@
+export const init = (app) => {
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "https://meva.thekingdave.com")
+        res.header("Access-Control-Allow-Origin", "http://localhost")
+        next()
+    })
+}
