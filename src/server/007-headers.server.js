6942155c3e82269706822ef180e7495b5a6ff46e
David Langheiter <david@langheiter.com>
2019-01-12 13:04:34 +0100

    Added localhost:3000 to cors

--- src/server/007-headers.server.js
+++ src/server/007-headers.server.js
@@ -1,7 +1,7 @@
 export const init = (app) => {
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "https://meva.thekingdave.com")
-        res.header("Access-Control-Allow-Origin", "http://localhost")
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000")
         next()
     })
 }
